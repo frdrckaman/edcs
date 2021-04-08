7fbcab429374ed90b8ee888490949769f8db7dfa
frdrckaman <frdrckaman@gmail.com>
2021-04-08 13:27:20 +0300

    Set up github Actions

---
+++ edcs_auth/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EdcsAuthConfig(AppConfig):
+    name = 'edcs_auth'
---
+++ edcs_dashboard/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EdcsDashboardConfig(AppConfig):
+    name = 'edcs_dashboard'
