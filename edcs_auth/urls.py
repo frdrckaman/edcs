2a40766009d1c5a019895f541d7736cf53d5edbe
frdrckaman <frdrckaman@gmail.com>
2021-04-09 06:49:08 +0300

    flake8 fix

--- edcs_auth/urls.py
+++ edcs_auth/urls.py
@@ -3,5 +3,6 @@ from .views import LoginView, LogoutView
 
 urlpatterns = [
     path('', LoginView.as_view(redirect_authenticated_user=True), name="login"),
-    path('logout/', LogoutView.as_view(template_name="edcs_auth/bootstrap3/login.html"), name="logout",),
+    path('logout/', LogoutView.as_view(template_name="edcs_auth/bootstrap3/login.html"),
+         name="logout",),
 ]
