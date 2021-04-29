9e3e1032087ace15f5281f12e86ae6df93eb5c8f
Maquiz1 <mjwinstone@yahoo.com>
2021-04-29 17:35:29 +0300

    Add edit to edcs_rab002

--- edcs_rab002/urls.py
+++ edcs_rab002/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib import admin
-from edcs_rab002.views import DemographicCreate
+from edcs_rab002.views import DemographicCreate, DemographicEdit
 
 
 urlpatterns = [
     path('DemographicCreate/', DemographicCreate, name='DemographicCreate'),
+    path('DemographicEdit/<int:pk>', DemographicEdit, name='DemographicEdit'),
     path('admin/', admin.site.urls),
 ]
--- edcs_rab002/views.py
+++ edcs_rab002/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import DemographicForm
+from .models import Demographic
 
 
 def DemographicCreate(request):
@@ -11,3 +12,16 @@ def DemographicCreate(request):
     else:
         form = DemographicForm()
     return render(request, 'edcs_rab002/edcs_rab002_home.html', {'form': form})
+
+
+def DemographicEdit(request, pk=None):
+    demographic = get_object_or_404(Demographic, pk=pk)
+    if request.method == 'POST':
+        form = DemographicForm(request.POST, instance=demographic)
+        if form.is_valid():
+            form.save()
+            return redirect('edcs_rab002/DemographicEdit')
+    else:
+        form = DemographicForm(instance=demographic)
+    return render(request, 'edcs_rab002/edcs_rab002_edit.html', {'form': form, 'demographic': demographic})
+
