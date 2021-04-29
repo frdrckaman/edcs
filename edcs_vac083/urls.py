a4eba514fa08b994dc0d8bd9ad9a411926672003
Maquiz1 <mjwinstone@yahoo.com>
2021-04-29 11:19:51 +0300

    Add vac083 button to EDCS_DASHBOARD

--- edcs_vac083/urls.py
+++ edcs_vac083/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from . import views
+from edcs_vac083_home.vi
 
 
 app_name = 'edcs_vac083'
 
 urlpatterns = [
-    path('', views.edcs_vac083_home, name='edcs_vac083_home'),
+    # path('', views.edcs_vac083_home, name='edcs_vac083_home'),
+    path('vac083/home/', edcs_vac083_home.as_view(template_name='edcs_vac083/edcs_vac083_home.html'),
+         name='edcs_vac083_home'),
 ]
