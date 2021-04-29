4264a6af8a82bbac3bd748a63639c89980ff6e6c
Maquiz1 <mjwinstone@yahoo.com>
2021-04-29 12:19:36 +0300

    add vac083 view function

--- edcs_vac083/urls.py
+++ edcs_vac083/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from edcs_vac083_home.vi
+from edcs_vac083.views import edcs_vac083Home
 
 
 app_name = 'edcs_vac083'
 
 urlpatterns = [
-    # path('', views.edcs_vac083_home, name='edcs_vac083_home'),
-    path('vac083/home/', edcs_vac083_home.as_view(template_name='edcs_vac083/edcs_vac083_home.html'),
-         name='edcs_vac083_home'),
+    path('', edcs_vac083Home, name="vac083"),
 ]
--- edcs_vac083/views.py
+++ edcs_vac083/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+from django.urls import reverse
+from django.views import generic
 
 
-def edcs_vac083_home(request):
-    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-    return HttpResponse("Hello, world. You're at the Home Page.")
+# def edcs_vac083_home(generic.ListView):
+#     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # output = ', '.join([q.question_text for q in latest_question_list])
+#     # return HttpResponse(output)
+#     template_name = 'polls/index.html'
+#     context_object_name = 'latest_question_list'
+#     # return HttpResponse("Hello, world. You're at the Home Page.")
+#
+# def edcs_vac083_home(request):
+#      return HttpResponse("Hello, world. You're at the Home Page.")
+
+
+def edcs_vac083Home(request):
+    # template_name = 'edcs_vac083/edcs_vac083_home.html'
+    # context_object_name = 'latest_question_list'
+    return HttpResponse('HELO HELO')
