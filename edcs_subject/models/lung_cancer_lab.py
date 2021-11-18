1e7a5cf8d53468e76a20b6ee8ee80b8b542aca0f
frdrckaman <frdrckaman@gmail.com>
2021-11-18 14:41:57 +0300

    LungCancerLabInvestigation model

---
+++ edcs_subject/models/lung_cancer_lab.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+from edcs_constants.choices import YES_NO
+from edcs_model import models as edcs_models
+
+
+class LungCancerLabInvestigation(
+    edcs_models.BaseUuidModel,
+):
+
+    biopsy_tissues_mutation = models.CharField(
+        verbose_name="From the biopsy tissues, is there any mutation?",
+        max_length=45,
+        choices=YES_NO,
+    )
+
+    mutation_detected = models.CharField(
+        verbose_name="If yes, what mutations were detected?",
+        max_length=125,
+        blank=True,
+        null=True,
+    )
+
+    dna_methylation_age = models.IntegerField(
+        verbose_name="Was the DNA methylation age done?", max_length=45, choices=YES_NO
+    )
+
+    dna_methylation_result = models.CharField(
+        verbose_name="If yes, what were the DNAm results?",
+        max_length=125,
+        blank=True,
+        null=True,
+    )
+
+    brock_score = models.CharField(
+        verbose_name="What was the patient’s Brock score?",
+        max_length=45,
+        blank=True,
+        null=True,
+    )
+    lungrads_score = models.CharField(
+        verbose_name="What was the patient’s LungRADS score?",
+        max_length=45,
+        blank=True,
+        null=True,
+    )
+    luniris_score = models.CharField(
+        verbose_name="What was the patient’s LunIRiS score?",
+        max_length=45,
+        blank=True,
+        null=True,
+    )
+
+    class Meta(edcs_models.BaseUuidModel.Meta):
+        verbose_name = "Lung Cancer Lab Investigation"
+        verbose_name_plural = "Lung Cancer Lab Investigation"
