b8cc1b8f68a4f1d4e939c3a8459e37259454139e
frdrckaman <frdrckaman@gmail.com>
2021-11-21 13:12:41 +0300

    LungCancerTreatmentAdmin

---
+++ edcs_subject/admin/lung_cancer_treatment_admin.py
@@ -0,0 +1,62 @@
+from django.contrib import admin
+from django_audit_fields import audit_fieldset_tuple
+from edcs_model_admin import SimpleHistoryAdmin
+
+from ..admin_site import edcs_subject_admin
+from ..models import LungCancerTreatment
+
+
+@admin.register(LungCancerTreatment, site=edcs_subject_admin)
+class LungCancerTreatmentAdmin(SimpleHistoryAdmin):
+    fieldsets = (
+        [
+            None,
+            {
+                "fields": (
+                    "report_datetime",
+                ),
+            },
+         ],
+        [
+            "LUNG CANCER TREATMENT",
+            {
+                "fields": (
+                    "lung_cancer_stage",
+                    "date_start_treatment",
+                    "treatment",
+                    "treatment_other",
+                ),
+            },
+        ],
+
+        audit_fieldset_tuple,
+    )
+
+    list_display = (
+        "report_datetime",
+        "lung_cancer_stage",
+        "date_start_treatment",
+        "treatment",
+        "treatment_other",
+        "created",
+    )
+
+    list_filter = (
+        "report_datetime",
+        "lung_cancer_stage",
+        "date_start_treatment",
+        "treatment",
+        "treatment_other",
+    )
+
+    search_fields = (
+        "report_datetime",
+    )
+
+    radio_fields = {
+        "lung_cancer_stage": admin.VERTICAL,
+        "treatment": admin.VERTICAL,
+    }
+
+    def post_url_on_delete_kwargs(self, request, obj):
+        return {}
