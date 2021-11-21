f914ec09e8c247f48dc254e86d3c865472e9304e
frdrckaman <frdrckaman@gmail.com>
2021-11-21 10:37:10 +0300

    ContraceptiveUseReproductiveHistoryAdmin

---
+++ edcs_subject/admin/contraceptive_reproductive_admin.py
@@ -0,0 +1,81 @@
+from django.contrib import admin
+from django_audit_fields import audit_fieldset_tuple
+from edcs_model_admin import SimpleHistoryAdmin
+
+from ..admin_site import edcs_subject_admin
+from ..models import ContraceptiveUseReproductiveHistory
+
+
+@admin.register(ContraceptiveUseReproductiveHistory, site=edcs_subject_admin)
+class ContraceptiveUseReproductiveHistoryAdmin(SimpleHistoryAdmin):
+    fieldsets = (
+        [
+            None,
+            {
+                "fields": (
+                    "report_datetime",
+                ),
+            },
+         ],
+        [
+            "CONTRACEPTIVE USE AND REPRODUCTIVE HISTORY",
+            {
+                "fields": (
+                    "age_attain_menarche",
+                    "age_have_first_child",
+                    "age_have_last_child",
+                    "breast_feed",
+                    "use_oral_contraceptives",
+                    "how_long_use_oral_contraceptives",
+                    "when_stop_use_contraceptives",
+                    "post_menopausal_hormone_therapy",
+                    "how_long_post_menopausal_hormone_therapy",
+                    "how_long_stop_post_menopausal_hormone_therapy",
+                    "age_attain_menopause",
+                ),
+            },
+        ],
+        audit_fieldset_tuple,
+    )
+
+    list_display = (
+        "report_datetime",
+        "age_attain_menarche",
+        "age_have_last_child",
+        "breast_feed",
+        "use_oral_contraceptives",
+        "how_long_use_oral_contraceptives",
+        "age_attain_menopause",
+        "created",
+    )
+
+    list_filter = (
+        "report_datetime",
+        "age_attain_menarche",
+        "age_have_last_child",
+        "breast_feed",
+        "use_oral_contraceptives",
+        "how_long_use_oral_contraceptives",
+        "age_attain_menopause",
+    )
+
+    search_fields = (
+        "report_datetime",
+    )
+
+    radio_fields = {
+        "age_attain_menarche": admin.VERTICAL,
+        "age_have_first_child": admin.VERTICAL,
+        "age_have_last_child": admin.VERTICAL,
+        "breast_feed": admin.VERTICAL,
+        "use_oral_contraceptives": admin.VERTICAL,
+        "how_long_use_oral_contraceptives": admin.VERTICAL,
+        "when_stop_use_contraceptives": admin.VERTICAL,
+        "post_menopausal_hormone_therapy": admin.VERTICAL,
+        "how_long_post_menopausal_hormone_therapy": admin.VERTICAL,
+        "how_long_stop_post_menopausal_hormone_therapy": admin.VERTICAL,
+        "age_attain_menopause": admin.VERTICAL,
+    }
+
+    def post_url_on_delete_kwargs(self, request, obj):
+        return {}
