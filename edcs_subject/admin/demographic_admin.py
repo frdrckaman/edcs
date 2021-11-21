bd1da91d37e9fe5cd11e3790b58b77b46b23beaf
frdrckaman <frdrckaman@gmail.com>
2021-11-21 11:49:36 +0300

    DemographicCharacteristicAdmin

---
+++ edcs_subject/admin/demographic_admin.py
@@ -0,0 +1,101 @@
+from django.contrib import admin
+from django_audit_fields import audit_fieldset_tuple
+from edcs_model_admin import SimpleHistoryAdmin
+
+from ..admin_site import edcs_subject_admin
+from ..models import DemographicCharacteristic
+
+
+@admin.register(DemographicCharacteristic, site=edcs_subject_admin)
+class DemographicCharacteristicAdmin(SimpleHistoryAdmin):
+    fieldsets = (
+        [
+            None,
+            {
+                "fields": (
+                    "report_datetime",
+                ),
+            },
+         ],
+        [
+            "DEMOGRAPHIC CHARACTERISTIC",
+            {
+                "fields": (
+                    "martial_status",
+                    "education",
+                    "education_other",
+                    "occupation",
+                    "occupation_other",
+                ),
+            },
+        ],
+        [
+            "SOCIAL-ECONOMIC CHARACTERISTICS",
+            {
+                "fields": (
+                    "electricity",
+                    "television",
+                    "radio",
+                    "an_iron",
+                    "bank_account",
+                    "material_build_floor",
+                    "material_build_floor_other",
+                    "material_build_walls",
+                    "material_build_walls_other",
+                    "material_build_roof",
+                    "material_build_roof_other",
+                    "use_in_cooking",
+                    "use_in_cooking_other",
+                    "main_power_source",
+                    "main_power_source_other",
+                    "household_monthly_income",
+                ),
+            },
+        ],
+
+        audit_fieldset_tuple,
+    )
+
+    list_display = (
+        "martial_status",
+        "education",
+        "occupation",
+        "electricity",
+        "television",
+        "bank_account",
+        "use_in_cooking",
+        "created",
+    )
+
+    list_filter = (
+        "martial_status",
+        "education",
+        "occupation",
+        "electricity",
+        "television",
+        "bank_account",
+        "use_in_cooking",
+    )
+
+    search_fields = (
+        "report_datetime",
+    )
+
+    radio_fields = {
+        "martial_status": admin.VERTICAL,
+        "education": admin.VERTICAL,
+        "occupation": admin.VERTICAL,
+        "electricity": admin.VERTICAL,
+        "television": admin.VERTICAL,
+        "radio": admin.VERTICAL,
+        "an_iron": admin.VERTICAL,
+        "bank_account": admin.VERTICAL,
+        "material_build_floor": admin.VERTICAL,
+        "material_build_walls": admin.VERTICAL,
+        "material_build_roof": admin.VERTICAL,
+        "use_in_cooking": admin.VERTICAL,
+        "main_power_source": admin.VERTICAL,
+    }
+
+    def post_url_on_delete_kwargs(self, request, obj):
+        return {}
