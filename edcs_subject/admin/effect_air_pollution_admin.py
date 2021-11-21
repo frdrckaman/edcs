f02b528e20e23833494e5751aecf19930776892c
frdrckaman <frdrckaman@gmail.com>
2021-11-21 12:03:43 +0300

    EffectAirPollutionAdmin

---
+++ edcs_subject/admin/effect_air_pollution_admin.py
@@ -0,0 +1,64 @@
+from django.contrib import admin
+from django_audit_fields import audit_fieldset_tuple
+from edcs_model_admin import SimpleHistoryAdmin
+
+from ..admin_site import edcs_subject_admin
+from ..models import EffectAirPollution
+
+
+@admin.register(EffectAirPollution, site=edcs_subject_admin)
+class EffectAirPollutionAdmin(SimpleHistoryAdmin):
+    fieldsets = (
+        [
+            None,
+            {
+                "fields": (
+                    "report_datetime",
+                ),
+            },
+         ],
+        [
+            "EFFECT OF AIR POLLUTION",
+            {
+                "fields": (
+                    "family_member_sickness",
+                    "who_had_illness",
+                    "fuel_before_changing",
+                    "variation_btn_fuel",
+                    "influence_variation_btn_fuel",
+                ),
+            },
+        ],
+
+        audit_fieldset_tuple,
+    )
+
+    list_display = (
+        "family_member_sickness",
+        "who_had_illness",
+        "fuel_before_changing",
+        "variation_btn_fuel",
+        "influence_variation_btn_fuel",
+    )
+
+    list_filter = (
+        "report_datetime",
+        "family_member_sickness",
+        "who_had_illness",
+        "fuel_before_changing",
+        "variation_btn_fuel",
+        "influence_variation_btn_fuel",
+    )
+
+    search_fields = (
+        "report_datetime",
+    )
+
+    radio_fields = {
+        "family_member_sickness": admin.VERTICAL,
+        "who_had_illness": admin.VERTICAL,
+        "variation_btn_fuel": admin.VERTICAL,
+    }
+
+    def post_url_on_delete_kwargs(self, request, obj):
+        return {}
