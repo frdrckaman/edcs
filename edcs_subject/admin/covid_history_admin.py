6a87abac4ea87feef95714f0ca5ed6c9a1b0bc10
frdrckaman <frdrckaman@gmail.com>
2021-11-21 11:31:25 +0300

    CovidInfectionHistory

---
+++ edcs_subject/admin/covid_history_admin.py
@@ -0,0 +1,81 @@
+from django.contrib import admin
+from django_audit_fields import audit_fieldset_tuple
+from edcs_model_admin import SimpleHistoryAdmin
+
+from ..admin_site import edcs_subject_admin
+from ..models import CovidInfectionHistory
+
+
+@admin.register(CovidInfectionHistory, site=edcs_subject_admin)
+class CovidInfectionHistoryAdmin(SimpleHistoryAdmin):
+    fieldsets = (
+        [
+            None,
+            {
+                "fields": (
+                    "report_datetime",
+                ),
+            },
+         ],
+        [
+            "COVID-19 INFECTION HISTORY",
+            {
+                "fields": (
+                    "think_had_covid",
+                    "date_think_had_covid",
+                    "have_covid_symptoms",
+                    "covid_symptoms",
+                    "admitted_hospital",
+                    "swab_test",
+                    "swab_test_results",
+                    "date_first_positive_test",
+                    "date_last_negative_test",
+                    "swab_test_results",
+                    "covid_vaccine",
+                    "vaccine_provider",
+                    "no_covid_vaccine",
+                    "date_recent_vaccination",
+                ),
+            },
+        ],
+
+        audit_fieldset_tuple,
+    )
+
+    list_display = (
+        "report_datetime",
+        "think_had_covid",
+        "swab_test",
+        "swab_test_results",
+        "swab_test_results",
+        "no_covid_vaccine",
+    )
+
+    list_filter = (
+        "report_datetime",
+        "think_had_covid",
+        "swab_test",
+        "swab_test_results",
+        "swab_test_results",
+        "no_covid_vaccine",
+    )
+
+    search_fields = (
+        "report_datetime",
+    )
+
+    radio_fields = {
+        "think_had_covid": admin.VERTICAL,
+        "have_covid_symptoms": admin.VERTICAL,
+        "covid_symptoms": admin.VERTICAL,
+        "admitted_hospital": admin.VERTICAL,
+        "swab_test": admin.VERTICAL,
+        "swab_test_results": admin.VERTICAL,
+        "covid_vaccinated": admin.VERTICAL,
+        "covid_vaccine": admin.VERTICAL,
+        "vaccine_provider": admin.VERTICAL,
+        "no_covid_vaccine": admin.VERTICAL,
+    }
+
+    def post_url_on_delete_kwargs(self, request, obj):
+        return {}
