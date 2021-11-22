2a41661dbb4d5e12cd784b3225dc875b870ae801
frdrckaman <frdrckaman@gmail.com>
2021-11-22 11:37:26 +0300

    template modifications

--- edcs_dashboard/urls.py
+++ edcs_dashboard/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.contrib import admin
-from edcs_dashboard.views import HomeView
+from edcs_dashboard.views import HomeView, EnrollListView, EnrollDashboardView, CrfListView
 
 app_name = "edcs_dashboard"
 
 urlpatterns = [path("admin/", admin.site.urls)]
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
+    path('enroll-list/', EnrollListView.as_view(), name="enroll-list"),
+    path('enroll-dashboard/', EnrollDashboardView.as_view(), name='enroll-dashboard'),
+    path('crf-list/', CrfListView.as_view(), name='crf-list'),
 ]
--- edcs_dashboard/views/__init__.py
+++ edcs_dashboard/views/__init__.py
@@ -1,3 +1,6 @@
 from .administration_view import AdministrationView
 from .dashboard_view import DashboardView
 from .home_view import HomeView
+from .enroll_list_view import EnrollListView
+from .enroll_dashboard import EnrollDashboardView
+from .crf_list import CrfListView
