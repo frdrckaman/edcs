1d3d8e42ef4530a5253c4e75d4e69c45a69f91fa
frdrckaman <frdrckaman@gmail.com>
2022-01-07 12:38:27 +0300

    edcs_visit_schedule & edcs_appointment apps

---
+++ edcs_appointment/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EdcsAppointmentConfig(AppConfig):
+    name = 'edcs_appointment'
---
+++ edcs_visit_schedules/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EdcsVisitSchedulesConfig(AppConfig):
+    name = 'edcs_visit_schedules'
