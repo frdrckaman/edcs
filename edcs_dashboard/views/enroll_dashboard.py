a0eda4a419240a33235e4d827551a5168cd9b630
frdrckaman <frdrckaman@gmail.com>
2022-01-13 18:40:44 +0300

    remove unused code on enroll_dashboard

--- edcs_dashboard/views/enroll_dashboard.py
+++ edcs_dashboard/views/enroll_dashboard.py
@@ -1,5 +1,5 @@
 from pprint import pprint
-from django.apps import apps as django_apps
+
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -7,13 +7,8 @@ from edcs_dashboard.views.subject_list import SubjectDashboardView
 from edcs_visit_schedule.models.visit_schedule import VisitSchedule
 
 
-class ListboardViewError(Exception):
-    pass
-
-
 class EnrollDashboardView(SubjectDashboardView):
     template_name = f"edcs_dashboard/bootstrap{settings.EDCS_BOOTSTRAP}/enroll_dashboard.html"
-    # listboard_model = "edcs_appointment.appointment"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
