005035770962026c1035970d9559c44d955e016a
frdrckaman <frdrckaman@gmail.com>
2022-01-13 18:10:19 +0300

    enroll_dashboard view & template

--- edcs_dashboard/views/enroll_dashboard.py
+++ edcs_dashboard/views/enroll_dashboard.py
@@ -1,25 +1,28 @@
 from pprint import pprint
-
+from django.apps import apps as django_apps
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from edcs_dashboard.views.subject_list import SubjectDashboardView
+from edcs_visit_schedule.models.visit_schedule import VisitSchedule
+
+
+class ListboardViewError(Exception):
+    pass
 
 
 class EnrollDashboardView(SubjectDashboardView):
     template_name = f"edcs_dashboard/bootstrap{settings.EDCS_BOOTSTRAP}/enroll_dashboard.html"
+    # listboard_model = "edcs_appointment.appointment"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context.update(
-        #     edc_packages=["not available"],
-        #     third_party_packages=["not available"],
-        #     installed_apps=settings.INSTALLED_APPS,
-        # )
-        # pprint(self.kwargs['subject'])
+        context.update(
+            appointments=VisitSchedule.objects.all(),
+        )
+        # pprint(self.listboard_model_cls().get_absolute_url())
         return context
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
-
