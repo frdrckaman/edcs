52dd7d924097d873cbfb0c8c18cd15b3fe8192e2
frdrckaman <frdrckaman@gmail.com>
2022-01-19 16:51:34 +0300

    start_visit_button templatetag

--- edcs_dashboard/templatetags/subject_dashboard_extras.py
+++ edcs_dashboard/templatetags/subject_dashboard_extras.py
@@ -1,6 +1,10 @@
 import datetime
+from pprint import pprint
+
 from django import template
 from django.conf import settings
+
+from edcs_appointment.models import Appointment
 from edcs_utils import age
 
 register = template.Library()
@@ -17,7 +21,7 @@ def side_info_panel(result):
         dob=result.dob.strftime("%Y-%m-%d"),
         age=age(result.dob, datetime.datetime.now()).years,
         initials=result.initials,
-        identity=result.identity
+        identity=result.identity,
     )
 
 
@@ -30,3 +34,27 @@ def consent_panel(result):
         href=result,
         title=title,
     )
+
+
+@register.inclusion_tag(
+    f"edcs_dashboard/bootstrap{settings.EDCS_BOOTSTRAP}/"
+    f"buttons/start_visit_button.html",
+    takes_context=True,
+)
+def start_visit(context, visit):
+    listboard_dashboard = "edcs_dashboard:enroll-dashboard"
+    title = "Start Subject's Visit"
+
+    appointment = Appointment.objects.get(
+        subject_identifier=context.get("subject"), visit_code=visit
+    )
+    nxt = listboard_dashboard + "&subject=" + context.get("subject")
+
+    return dict(
+        href=next_url(appointment.admin_url(appointment.id), nxt),
+        title=title,
+    )
+
+
+def next_url(url, nxt):
+    return "?next=".join([url, nxt])
