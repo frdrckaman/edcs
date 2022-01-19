4c4531166b99e5d9c5bdb4d7f5ff5fc292ad2957
frdrckaman <frdrckaman@gmail.com>
2022-01-19 13:35:50 +0300

    schedule dictionary

--- edcs_consent/models/signals.py
+++ edcs_consent/models/signals.py
@@ -1,6 +1,10 @@
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
+
+from edcs_appointment.models import Appointment
 from edcs_screening.models import SubjectScreening
+from edcs_visit_schedule.models import VisitSchedule
+
 from .subject_consent import SubjectConsent
 
 
@@ -21,7 +25,20 @@ def subject_consent_on_post_save(sender, instance, raw, created, **kwargs):
 
             subject_screening.subject_identifier = instance.subject_identifier
             subject_screening.consented = True
-            subject_screening.save_base(update_fields=["subject_identifier", "consented"])
+            subject_screening.save_base(
+                update_fields=["subject_identifier", "consented"]
+            )
+
+            schedules = VisitSchedule.objects.all()
+
+            for schedule in schedules:
+                visit_schedule = {
+                    "subject_identifier": instance.subject_identifier,
+                    "visit_schedule_name": schedule.visit_schedule_name,
+                    "schedule_name": schedule.schedule_name,
+                    "visit_code": schedule.visit_code,
+                    "timepoint": schedule.timepoint,
+                }
 
 
 @receiver(
