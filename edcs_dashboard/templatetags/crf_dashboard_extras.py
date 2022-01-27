2702bc8a1bea6f5e859a6a6e3231f102b3150498
frdrckaman <frdrckaman@gmail.com>
2022-01-27 21:39:58 +0300

    add_edit_button url

--- edcs_dashboard/templatetags/crf_dashboard_extras.py
+++ edcs_dashboard/templatetags/crf_dashboard_extras.py
@@ -1,16 +1,34 @@
+from pprint import pprint
+
 from django import template
 from django.conf import settings
+from django.urls import reverse
+
+from edcs_subject.models import SubjectVisit
 
 register = template.Library()
 
 
+def next_url(url, nxt):
+    return "?next=".join([url, nxt])
+
+
 @register.inclusion_tag(
-    f"edcs_dashboard/bootstrap{settings.EDCS_BOOTSTRAP}/" f"menu/add_edit_crf_button.html",
+    f"edcs_dashboard/bootstrap{settings.EDCS_BOOTSTRAP}/" f"buttons/add_edit_crf_button.html",
     takes_context=True,
 )
-def add_edit_crf(context):
+def add_edit_crf(context, url):
+    listboard_dashboard = "edcs_dashboard:crf-list"
+    subject_identifier = context.get("subject")
+    appointment = context.get("appointment")
+
+    subject_visit = SubjectVisit.objects.get(appointment_id=appointment)
+    nxt = listboard_dashboard + "&subject=" + subject_identifier + "&appointment=" + appointment + "subject_visit=" + str(subject_visit.id)
+
+    pprint(next_url(reverse(url), nxt))
+
     title = "Add"
     return dict(
         title=title,
+        href=next_url(reverse(url), nxt)
     )
-
