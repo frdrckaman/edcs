02be99eed9611beff04919f44066593db59676a6
frdrckaman <frdrckaman@gmail.com>
2022-01-28 09:38:56 +0300

    add CrfStatusModelAdminMixin to CrfModelAdminMixin

--- edcs_subject/admin/modeladmin_mixins.py
+++ edcs_subject/admin/modeladmin_mixins.py
@@ -1,4 +1,4 @@
-# from edcs_crf.admin import CrfStatusModelAdminMixin, crf_status_fieldset_tuple
+from edcs_crf.admin import CrfStatusModelAdminMixin, crf_status_fieldset_tuple
 from edcs_model_admin import SimpleHistoryAdmin
 from edcs_model_admin.dashboard import (
     ModelAdminCrfDashboardMixin,
@@ -10,7 +10,7 @@ class ModelAdminMixin(ModelAdminSubjectDashboardMixin):
     pass
 
 
-class CrfModelAdminMixin(ModelAdminCrfDashboardMixin):
+class CrfModelAdminMixin(CrfStatusModelAdminMixin, ModelAdminCrfDashboardMixin):
     pass
 
 
