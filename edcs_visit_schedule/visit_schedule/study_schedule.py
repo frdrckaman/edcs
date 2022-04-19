c30e8ee69d7d7a03d604db8d4e7db577c42922c2
frdrckaman <frdrckaman@gmail.com>
2022-04-19 16:07:23 +0300

    update visit schedule

--- edcs_visit_schedule/visit_schedule/study_schedule.py
+++ edcs_visit_schedule/visit_schedule/study_schedule.py
@@ -29,6 +29,7 @@ visit06 = {
     "schedule_name": "subject_followup",
     "timepoint": 6,
 }
+visits.append(visit06)
 
 visit09 = {
     "visit_code": "1009",
@@ -38,5 +39,174 @@ visit09 = {
     "schedule_name": "subject_followup",
     "timepoint": 9,
 }
-
 visits.append(visit09)
+
+visit12 = {
+    "visit_code": "1012",
+    "visit_name": "1012",
+    "visit_title": "Month 12",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 12,
+}
+visits.append(visit12)
+
+visit15 = {
+    "visit_code": "1015",
+    "visit_name": "1015",
+    "visit_title": "Month 15",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 15,
+}
+visits.append(visit15)
+
+visit18 = {
+    "visit_code": "1018",
+    "visit_name": "1018",
+    "visit_title": "Month 18",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 18,
+}
+visits.append(visit18)
+
+visit21 = {
+    "visit_code": "1021",
+    "visit_name": "1021",
+    "visit_title": "Month 21",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 21,
+}
+visits.append(visit21)
+
+visit24 = {
+    "visit_code": "1024",
+    "visit_name": "1024",
+    "visit_title": "Month 24",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 24,
+}
+visits.append(visit24)
+
+visit27 = {
+    "visit_code": "1027",
+    "visit_name": "1027",
+    "visit_title": "Month 27",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 27,
+}
+visits.append(visit27)
+
+visit30 = {
+    "visit_code": "1030",
+    "visit_name": "1030",
+    "visit_title": "Month 30",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 30,
+}
+visits.append(visit30)
+
+visit33 = {
+    "visit_code": "1033",
+    "visit_name": "1033",
+    "visit_title": "Month 33",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 33,
+}
+visits.append(visit33)
+
+visit36 = {
+    "visit_code": "1036",
+    "visit_name": "1036",
+    "visit_title": "Month 36",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 36,
+}
+visits.append(visit36)
+
+visit39 = {
+    "visit_code": "1039",
+    "visit_name": "1039",
+    "visit_title": "Month 39",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 39,
+}
+visits.append(visit39)
+
+visit42 = {
+    "visit_code": "1042",
+    "visit_name": "1042",
+    "visit_title": "Month 42",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 42,
+}
+visits.append(visit42)
+
+visit45 = {
+    "visit_code": "1045",
+    "visit_name": "1045",
+    "visit_title": "Month 45",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 45,
+}
+visits.append(visit45)
+
+visit48 = {
+    "visit_code": "1048",
+    "visit_name": "1048",
+    "visit_title": "Month 48",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 48,
+}
+visits.append(visit48)
+
+visit51 = {
+    "visit_code": "1051",
+    "visit_name": "1051",
+    "visit_title": "Month 51",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 51,
+}
+visits.append(visit51)
+
+visit54 = {
+    "visit_code": "1054",
+    "visit_name": "1054",
+    "visit_title": "Month 54",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 54,
+}
+visits.append(visit54)
+
+visit57 = {
+    "visit_code": "1057",
+    "visit_name": "1057",
+    "visit_title": "Month 57",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 57,
+}
+visits.append(visit57)
+
+visit60 = {
+    "visit_code": "1060",
+    "visit_name": "1060",
+    "visit_title": "Month 60",
+    "visit_schedule_name": "visit_schedule",
+    "schedule_name": "subject_followup",
+    "timepoint": 60,
+}
+visits.append(visit60)
