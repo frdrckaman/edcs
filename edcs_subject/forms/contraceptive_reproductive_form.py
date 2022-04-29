85faa8270f923b128f3e65487eb5fa8d252eee25
frdrckaman <frdrckaman@gmail.com>
2022-04-29 14:11:54 +0300

    update ContraceptiveUseReproductiveHistoryFormValidator

--- edcs_subject/forms/contraceptive_reproductive_form.py
+++ edcs_subject/forms/contraceptive_reproductive_form.py
@@ -1,6 +1,11 @@
 from django import forms
-from edcs_constants.constants import YES, YES_CURRENT_USER, YES_PAST_USER, NOT_APPLICABLE
 
+from edcs_constants.constants import (
+    NOT_APPLICABLE,
+    YES,
+    YES_CURRENT_USER,
+    YES_PAST_USER,
+)
 from edcs_form_validators import FormValidatorMixin
 from edcs_form_validators.form_validator import FormValidator
 
@@ -8,28 +13,29 @@ from ..models import ContraceptiveUseReproductiveHistory
 
 
 class ContraceptiveUseReproductiveHistoryFormValidator(FormValidator):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
+    def __init__(self):
+        super().__init__()
         self.use_oral_contraceptives = self.cleaned_data.get("use_oral_contraceptives")
-        self.how_long_use_oral_contraceptives = self.cleaned_data.get("how_long_use_oral_contraceptives")
+        self.how_long_use_oral_contraceptives = self.cleaned_data.get(
+            "how_long_use_oral_contraceptives"
+        )
 
     def clean(self):
-        super().clean()
-
         self.validate_how_long_use_oral_contraceptives()
 
-        self.applicable_if(YES, field="post_menopausal_hormone_therapy",
-                           field_applicable="how_long_post_menopausal_hormone_therapy")
+        self.applicable_if(
+            YES,
+            field="post_menopausal_hormone_therapy",
+            field_applicable="how_long_post_menopausal_hormone_therapy",
+        )
 
     def validate_how_long_use_oral_contraceptives(self):
-        if (self.use_oral_contraceptives == YES_CURRENT_USER or self.use_oral_contraceptives == YES_PAST_USER) \
-                and self.how_long_use_oral_contraceptives == NOT_APPLICABLE:
+        if (
+            self.use_oral_contraceptives == YES_CURRENT_USER
+            or self.use_oral_contraceptives == YES_PAST_USER
+        ) and self.how_long_use_oral_contraceptives == NOT_APPLICABLE:
             raise forms.ValidationError(
-                {
-                    "how_long_use_oral_contraceptives": "This field is Applicable "
-                }
+                {"how_long_use_oral_contraceptives": "This field is Applicable "}
             )
 
 
