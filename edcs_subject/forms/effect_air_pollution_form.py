bf2af00f26943b0e1f70b4bc0c90ea59658444bc
frdrckaman <frdrckaman@gmail.com>
2022-05-03 07:36:34 +0300

    update EffectAirPollutionFormValidator

--- edcs_subject/forms/effect_air_pollution_form.py
+++ edcs_subject/forms/effect_air_pollution_form.py
@@ -1,6 +1,6 @@
 from django import forms
-from edcs_constants.constants import OTHER, YES
 
+from edcs_constants.constants import OTHER, YES
 from edcs_form_validators import FormValidatorMixin
 from edcs_form_validators.form_validator import FormValidator
 
@@ -8,16 +8,19 @@ from ..models import EffectAirPollution
 
 
 class EffectAirPollutionFormValidator(FormValidator):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
     def clean(self):
-        super().clean()
 
-        self.applicable_if(YES, field="family_member_sickness", field_applicable="who_had_illness")
-        self.required_if(YES, field="family_member_sickness", field_required="fuel_before_changing")
-        self.required_if(YES, field="variation_btn_fuel", field_required="influence_variation_btn_fuel")
+        self.applicable_if(
+            YES, field="family_member_sickness", field_applicable="who_had_illness"
+        )
+        self.required_if(
+            YES, field="family_member_sickness", field_required="fuel_before_changing"
+        )
+        self.required_if(
+            YES,
+            field="variation_btn_fuel",
+            field_required="influence_variation_btn_fuel",
+        )
 
 
 class EffectAirPollutionForm(FormValidatorMixin, forms.ModelForm):
