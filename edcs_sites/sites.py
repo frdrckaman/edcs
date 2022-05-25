c68597608936db73a5a68f0a2891af53b5c21fe9
frdrckaman <frdrckaman@gmail.com>
2022-05-25 14:37:00 +0300

    clean codes

--- edcs_sites/sites.py
+++ edcs_sites/sites.py
@@ -12,20 +12,6 @@ default_country = getattr(settings, "EDCS_SITES_DEFAULT_COUNTRY", "tanzania")
 default_country_code = getattr(settings, "EDCS_SITES_DEFAULT_COUNTRY_CODE", "tz")
 default_domain = getattr(settings, "EDCS_SITES_DEFAULT_DOMAIN", "localhost")
 
-# all_sites = {
-#     default_country
-#     or "site": [
-#         SingleSite(
-#             1,
-#             settings.APP_NAME,
-#             country=default_country,
-#             country_code=default_country_code,
-#             domain=default_domain,
-#         ),
-#     ],
-# }
-
-# fqdn = "edcs.org"
 fqdn = settings.EDCS_DOMAIN
 edcs = ""
 
