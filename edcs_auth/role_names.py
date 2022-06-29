5abdc6d83d0d3f3d8c7b471d759d49f367a4e416
frdrckaman <frdrckaman@gmail.com>
2022-06-29 06:45:57 +0300

    add HISTOPATHOLOGY_ROLE,MOLECULAR_GENETICS_ROLE,HM_LOCATOR_AIR_MONITOR_ROLE

--- edcs_auth/role_names.py
+++ edcs_auth/role_names.py
@@ -5,6 +5,9 @@ from .constants import (
     CLINIC_SUPER_ROLE,
     CLINIC_ROLE,
     LABORATORY_ROLE,
+    HISTOPATHOLOGY_ROLE,
+    MOLECULAR_GENETICS_ROLE,
+    HM_LOCATOR_AIR_MONITOR_ROLE,
 
 )
 from .group_names import (
@@ -14,6 +17,9 @@ from .group_names import (
     CLINIC_SUPER,
     CLINIC,
     LABORATORY,
+    HISTOPATHOLOGY,
+    MOLECULAR_GENETICS,
+    HM_LOCATOR_AIR_MONITOR,
 )
 
 role_names = {
@@ -22,7 +28,10 @@ role_names = {
     CLINIC_ROLE: "Clinic",
     CUSTOM_ROLE: "Custom ...",
     STAFF_ROLE: "Staff",
-    LABORATORY_ROLE: "Laboratory"
+    LABORATORY_ROLE: "Laboratory",
+    HISTOPATHOLOGY_ROLE: "Histopathology",
+    MOLECULAR_GENETICS_ROLE: "Molecular genetics",
+    HM_LOCATOR_AIR_MONITOR_ROLE: "Home Locator Air Monitoring"
 }
 
 required_role_names = {STAFF_ROLE: "Staff"}
@@ -33,5 +42,8 @@ groups_by_role_name = {
     CLINIC_ROLE: [CLINIC, EVERYONE],
     STAFF_ROLE: [EVERYONE],
     LABORATORY_ROLE: [LABORATORY, EVERYONE],
+    HISTOPATHOLOGY_ROLE: [HISTOPATHOLOGY],
+    MOLECULAR_GENETICS_ROLE: [MOLECULAR_GENETICS],
+    HM_LOCATOR_AIR_MONITOR_ROLE: [HM_LOCATOR_AIR_MONITOR],
     CUSTOM_ROLE: [],
 }
