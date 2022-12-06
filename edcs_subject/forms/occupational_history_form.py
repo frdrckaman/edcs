70723e91c9a4173b3230c21a541162d042ddcd9b
frdrckaman <frdrckaman@gmail.com>
2022-12-06 06:06:47 +0300

    validate occupational_history

--- edcs_subject/forms/occupational_history_form.py
+++ edcs_subject/forms/occupational_history_form.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from edcs_constants.constants import OTHER, YES
+from edcs_constants.constants import NOT_APPLICABLE, OTHER, YES
 from edcs_form_validators import FormValidatorMixin
 from edcs_form_validators.form_validator import FormValidator
 
@@ -8,19 +8,52 @@ from ..models import OccupationalHistory
 
 
 class OccupationalHistoryFormValidator(FormValidator):
-    def clean(self):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-        self.applicable_if(
-            YES,
-            field="history_working_industries",
-            field_applicable="industries_worked",
+        self.history_working_industries = self.cleaned_data.get("history_working_industries")
+        self.industries_worked = (
+            self.cleaned_data.get("industries_worked").filter(name=NOT_APPLICABLE).exists()
         )
+        self.industries_working = self.cleaned_data.get("industries_worked")
+
+    def clean(self):
+        # self.applicable_if(
+        #     YES,
+        #     field="history_working_industries",
+        #     field_applicable="industries_worked",
+        # )
         self.m2m_other_specify(
             OTHER, m2m_field="industries_worked", field_other="industries_worked_other"
         )
         self.applicable_if(
             YES, field="history_working_mines", field_applicable="how_long_work_mine"
         )
+        self.validate_industries_worked()
+        self.validate_industries_worked_na()
+        self.validate_industries_working_na()
+
+    def validate_industries_worked(self):
+        if self.history_working_industries == YES and self.industries_worked:
+            raise forms.ValidationError(
+                {"industries_worked": "Not applicable is not a valid choice "}
+            )
+
+    def validate_industries_worked_na(self):
+        if (self.industries_working.count() > 1) and self.history_working_industries != YES:
+            raise forms.ValidationError(
+                {"industries_worked": "Not applicable is the only Valid choice "}
+            )
+
+    def validate_industries_working_na(self):
+        if (
+            (self.industries_working.count() == 1)
+            and self.history_working_industries != YES
+            and not self.industries_worked
+        ):
+            raise forms.ValidationError(
+                {"industries_worked": "Not applicable is the only Valid choice "}
+            )
 
 
 class OccupationalHistoryForm(FormValidatorMixin, forms.ModelForm):
