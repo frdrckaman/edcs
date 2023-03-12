82e973cc244e4653ff63d6280a5b1fce1ec08faa
frdrckaman <frdrckaman@gmail.com>
2023-03-12 11:55:30 +0300

    update creteria

--- edcs_screening/eligibility.py
+++ edcs_screening/eligibility.py
@@ -1,6 +1,13 @@
 from django.utils.safestring import mark_safe
 
-from edcs_constants.constants import NO, OTHER_CANCER, TBD, YES
+from edcs_constants.constants import (
+    CANCER_FREE,
+    LUNG_CANCER_SUSPECT,
+    NO,
+    OTHER_CANCER,
+    TBD,
+    YES,
+)
 from edcs_utils.date import get_utcnow
 
 
@@ -24,13 +31,25 @@ def check_eligible_final(obj):
     """Updates model instance fields `eligible` and `reasons_ineligible`."""
     reasons_ineligible = []
 
-    if (
-        obj.tb_diagnosis == YES
-        or obj.malignancy == YES
-        or (obj.patient_category == OTHER_CANCER and obj.diagnosed_lung_cancer == YES)
+    # if (
+    #     obj.tb_diagnosis == YES
+    #     or obj.malignancy == YES
+    #     or (obj.patient_category == OTHER_CANCER and obj.diagnosed_lung_cancer == YES)
+    # ):
+    #     obj.eligible = False
+    #     # reasons_ineligible.append("Subject unsuitable")
+    if obj.patient_category == LUNG_CANCER_SUSPECT and obj.malignancy == YES:
+        obj.eligible = False
+    elif obj.patient_category == CANCER_FREE and (
+        obj.malignancy == YES
+        or obj.lung_cancer_suspect == YES
+        or obj.diagnosed_lung_cancer == YES
     ):
         obj.eligible = False
-        # reasons_ineligible.append("Subject unsuitable")
+    elif obj.patient_category == OTHER_CANCER and (
+        obj.diagnosed_lung_cancer == YES or obj.lung_cancer_suspect == YES
+    ):
+        obj.eligible = False
     else:
         obj.eligible = True if calculate_eligible_final(obj) == YES else False
 
@@ -39,10 +58,21 @@ def check_eligible_final(obj):
     else:
         if obj.tb_diagnosis == YES:
             reasons_ineligible.append("Patient have a positive TB diagnosis")
-        if obj.malignancy == YES:
+        if obj.malignancy == YES and (
+            obj.patient_category == LUNG_CANCER_SUSPECT or obj.patient_category == CANCER_FREE
+        ):
             reasons_ineligible.append("Patient had malignancy")
-        if obj.diagnosed_lung_cancer == YES:
+        if obj.diagnosed_lung_cancer == YES and (
+            obj.patient_category == CANCER_FREE or obj.patient_category == OTHER_CANCER
+        ):
             reasons_ineligible.append("Patient diagnosed with lung cancer")
+        if obj.lung_cancer_suspect == YES and (
+            obj.patient_category == CANCER_FREE or obj.patient_category == OTHER_CANCER
+        ):
+            reasons_ineligible.append(
+                "Patient been suspected to have lung cancer on the "
+                "basis of clinical presentation"
+            )
         if reasons_ineligible:
             obj.reasons_ineligible = "|".join(reasons_ineligible)
         else:
@@ -62,13 +92,15 @@ def calculate_eligible_final(obj):
         and obj.chest_infections in [YES, NO]
     ):
         eligible = (
-            obj.abnormal_chest_xrays == YES
-            or obj.non_resolving_infection == YES
-            or obj.lung_cancer_suspect == YES
-            or obj.cough == YES
-            or obj.long_standing_cough == YES
-            or obj.cough_blood == YES
-            or obj.chest_infections == YES
+            # obj.abnormal_chest_xrays == YES
+            # or obj.non_resolving_infection == YES
+            # or obj.lung_cancer_suspect == YES
+            # or obj.cough == YES
+            # or obj.long_standing_cough == YES
+            # or obj.cough_blood == YES
+            # or obj.chest_infections == YES
+            obj.tb_diagnosis == NO
+            and (obj.above_eighteen == YES or obj.screening_consent == YES)
         )
         return NO if not eligible else YES
     return TBD
