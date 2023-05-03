0f753509ca9bfb4d6b18d677ecb5e392f487ddd6
frdrckaman <frdrckaman@gmail.com>
2023-05-03 18:31:44 +0300

    updates

--- jobs.sh
+++ jobs.sh
@@ -3,7 +3,7 @@
 source .bashrc
 conda deactivate
 conda activate edcs
-rm -f /Users/frdrck/edcs/edcs/.data/edcs/data.zip
+rm -f /home/frdrck/edcs/.data/edcs/data.zip
 
-python /Users/frdrck/edcs/edcs/edcs_export/data.py
+python /home/frdrck/edcs/edcs_export/data.py
 #/home/frdrck/miniconda3/envs/edcs/bin/python /home/frdrck/edcs/edcs_export/data.py
