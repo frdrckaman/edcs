4800b2faa96869bf26a0fb657408223234f8dab1
frdrckaman <frdrckaman@gmail.com>
2023-05-04 01:34:30 +0300

    update live config

--- jobs.sh
+++ jobs.sh
@@ -4,5 +4,6 @@ PATH=/home/live/miniconda3/envs/live/bin:/home/live/miniconda3/condabin:/usr/loc
 conda deactivate
 conda activate edcs
 rm -f /home/live/edcs/.data/edcs/data.zip
+rm -f /home/live/edcs/.data/edcs/data/*.xlsx
 
 python /home/live/edcs/edcs_export/data.py
