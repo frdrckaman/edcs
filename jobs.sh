c2160d20f4627afa8d0b618e4eef7d892bf650e3
frdrckaman <frdrckaman@gmail.com>
2023-05-03 19:14:50 +0300

    updates

--- jobs.sh
+++ jobs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+PATH=/home/frdrck/miniconda3/envs/edcs/bin:/home/frdrck/miniconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
-source /home/frdrck/.bashrc
+#source /home/frdrck/.bashrc
 conda deactivate
 conda activate edcs
 rm -f /home/frdrck/edcs/.data/edcs/data.zip
