65647bfc3aa6a1f4eb621029a73d5afba6e565ef
frdrckaman <frdrckaman@gmail.com>
2023-05-03 17:54:29 +0300

    update permission

--- jobs.sh
+++ jobs.sh
@@ -3,4 +3,4 @@
 #conda deactivate
 #conda activate edcs
 #rm -f /Users/frdrck/edcs/edcs/.data/edcs/data.zip
-/home/frdrck/miniconda3/envs/edcs/bin/python /home/frdrck/edcs/edcs/edcs_export/data.py
+/home/frdrck/miniconda3/envs/edcs/bin/python /home/frdrck/edcs/edcs_export/data.py
