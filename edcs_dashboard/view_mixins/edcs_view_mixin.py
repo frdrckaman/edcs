731e1d98f4a9e2a3fb91c7bc64b84949e485db62
frdrckaman <frdrckaman@gmail.com>
2023-08-24 22:24:01 +0300

    remove debug warning

--- edcs_dashboard/view_mixins/edcs_view_mixin.py
+++ edcs_dashboard/view_mixins/edcs_view_mixin.py
@@ -3,11 +3,12 @@ import warnings
 from django.apps import apps as django_apps
 from django.conf import settings
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import ContextMixin
 from django_revision.views import RevisionMixin
-from django.contrib.auth.mixins import LoginRequiredMixin
-from edcs_sites.view_mixins import SiteViewMixin
+
 from edcs_protocol import Protocol
+from edcs_sites.view_mixins import SiteViewMixin
 
 from .message_view_mixin import MessageViewMixin
 from .template_request_context_mixin import TemplateRequestContextMixin
@@ -21,8 +22,7 @@ class EdcsViewMixin(
     TemplateRequestContextMixin,
     ContextMixin,
 ):
-    """Adds common template variables and warning messages.
-    """
+    """Adds common template variables and warning messages."""
 
     edcs_protocol_app = "edcs_protocol"
     edcs_device_app = "edcs_device"
@@ -42,9 +42,7 @@ class EdcsViewMixin(
             {
                 "copyright": getattr(protocol, "copyright", "copyright?"),
                 "device_id": getattr(edc_device_app_config, "device_id", "device_id?"),
-                "device_role": getattr(
-                    edc_device_app_config, "device_role", "device_role?"
-                ),
+                "device_role": getattr(edc_device_app_config, "device_role", "device_role?"),
                 "disclaimer": getattr(protocol, "disclaimer", "disclaimer?"),
                 "institution": getattr(protocol, "institution", "institution?"),
                 "license": getattr(protocol, "license", "license?"),
@@ -61,30 +59,33 @@ class EdcsViewMixin(
 
     def check_for_warning_messages(self, live_system=None):
         if settings.DEBUG:
-            messages.add_message(
-                self.request,
-                messages.ERROR,
-                (
-                    "This EDCS is running in DEBUG-mode. Use for testing only. "
-                    "Do not use this system for production data collection!"
-                ),
-            )
+            pass
+            # messages.add_message(
+            #     self.request,
+            #     messages.ERROR,
+            #     (
+            #         "This EDCS is running in DEBUG-mode. Use for testing only. "
+            #         "Do not use this system for production data collection!"
+            #     ),
+            # )
         elif not settings.DEBUG and not live_system:
-            messages.add_message(
-                self.request,
-                messages.WARNING,
-                (
-                    "This EDCS is for testing only. "
-                    "Do not use this system for production data collection!"
-                ),
-            )
+            pass
+            # messages.add_message(
+            #     self.request,
+            #     messages.WARNING,
+            #     (
+            #         "This EDCS is for testing only. "
+            #         "Do not use this system for production data collection!"
+            #     ),
+            # )
         try:
             if settings.WARNING_MESSAGE:
-                messages.add_message(
-                    self.request,
-                    messages.WARNING,
-                    settings.WARNING_MESSAGE,
-                    extra_tags="warning",
-                )
+                pass
+                # messages.add_message(
+                #     self.request,
+                #     messages.WARNING,
+                #     settings.WARNING_MESSAGE,
+                #     extra_tags="warning",
+                # )
         except AttributeError:
             pass
