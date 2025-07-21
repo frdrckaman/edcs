521379e8197c7dad519dcb4c8da60cb365fa6323
frdrckaman <frdrckaman@gmail.com>
2025-07-21 14:43:18 +0300

    update job.sh

--- jobs.sh
+++ jobs.sh
@@ -30,7 +30,7 @@ if [ -z "$PYTHON" ]; then
 fi
 
 # 4) cleanup old exports
-rm -f /home/live/edcs/.data/edcs/data.zip
+#rm -f /home/live/edcs/.data/edcs/data.zip
 #rm -f /home/live/edcs/.data/edcs/data/*.xlsx
 
 # 5) run your Django-export job
