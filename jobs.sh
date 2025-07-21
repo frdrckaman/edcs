4106004d60fdb66ce0ae94c5e43849e1386b7b9a
frdrckaman <frdrckaman@gmail.com>
2025-07-21 11:59:40 +0300

    update

--- jobs.sh
+++ jobs.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-PATH=/home/live/miniconda3/envs/live/bin:/home/live/miniconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+#PATH=/home/live/miniconda3/envs/live/bin:/home/live/miniconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+source /home/live/.bashrc
 
 conda deactivate
 conda activate edcs
+
 rm -f /home/live/edcs/.data/edcs/data.zip
 rm -f /home/live/edcs/.data/edcs/data/*.xlsx
 
-python /home/live/edcs/edcs_export/data.py
+cd /home/live/edcs/edcs_export
+
+python data.py
