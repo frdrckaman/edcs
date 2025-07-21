fa70f56c20fc3f43bc5bd0970a8a32517c3363e1
frdrckaman <frdrckaman@gmail.com>
2025-07-21 12:14:23 +0300

    update

--- jobs.sh
+++ jobs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#PATH=/home/live/miniconda3/envs/live/bin:/home/live/miniconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 source /home/live/.bashrc
+
+eval "$(/home/live/miniconda3/bin/conda shell.bash hook)"
 
 conda deactivate
 conda activate edcs
